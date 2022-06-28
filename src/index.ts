07feb8ae1e4cff651f97a410bca95a7c9cf7a3a5
CryptotechOU <info@cryptotech.ee>
2022-06-28 10:25:06 +0300

    HiveAPI and HiveFarms classes first implementation

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,39 @@
 
 'use strict'
 
-export const test = 'Hello world'
+import fetch from 'node-fetch'
+
+export const SCHEME = 'https://'
+export const HOST = 'api2.hiveos.farm'
+export const BASE_PATH = '/api/v2'
+
+export class HiveError { }
+
+export class HiveFarms {
+	api: HiveAPI
+
+	constructor(api: HiveAPI) {
+		this.api = api
+	}
+
+	async get() {
+		return this.api.get('/farms')
+	}
+}
+
+export class HiveAPI {
+	farms: HiveFarms
+
+	constructor() {
+		this.farms = new HiveFarms(this)
+	}
+
+	async get(endpoint: string) {
+		return fetch(SCHEME + HOST + BASE_PATH + endpoint)
+			.then(response => response.json())
+	}
+}
+
+export default HiveAPI
 
 // EOF
