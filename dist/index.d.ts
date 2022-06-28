0158d6ca459901a0fb011790e5757c01ed9b1688
CryptotechOU <info@cryptotech.ee>
2022-06-28 11:35:10 +0300

    Re-enabled declarations

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,12 @@
 export declare const SCHEME = "https://";
 export declare const HOST = "api2.hiveos.farm";
 export declare const BASE_PATH = "/api/v2";
-export declare namespace HiveResponse {
+export declare namespace HiveInterfaces {
+    interface HiveAuthorization {
+        access_token: string;
+        token_type: string;
+        expires_in: number;
+    }
     interface Farms {
     }
 }
@@ -10,12 +15,13 @@ export declare class HiveError {
 export declare class HiveFarms {
     api: HiveAPI;
     constructor(api: HiveAPI);
-    get(): Promise<HiveResponse.Farms>;
+    get(): Promise<HiveInterfaces.Farms>;
 }
 export declare class HiveAPI {
     farms: HiveFarms;
-    constructor();
-    get(endpoint: '/farms'): Promise<HiveResponse.Farms>;
+    authorization: HiveInterfaces.HiveAuthorization;
+    constructor(authorization: HiveInterfaces.HiveAuthorization);
+    get(endpoint: '/farms'): Promise<HiveInterfaces.Farms>;
     get(endpoint: string): Promise<object>;
 }
 export default HiveAPI;
