1dc09e64f36104655e85daa34c92868577bc9d0d
CryptotechOU <info@cryptotech.ee>
2022-06-28 11:13:59 +0300

    Release v0.0.6

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,13 @@
 // S.
 'use strict';
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HiveAPI = exports.HiveFarms = exports.HiveError = exports.BASE_PATH = exports.HOST = exports.SCHEME = void 0;
-const node_fetch_1 = __importDefault(require("node-fetch"));
-exports.SCHEME = 'https://';
-exports.HOST = 'api2.hiveos.farm';
-exports.BASE_PATH = '/api/v2';
-class HiveError {
+import fetch from 'node-fetch';
+export const SCHEME = 'https://';
+export const HOST = 'api2.hiveos.farm';
+export const BASE_PATH = '/api/v2';
+export class HiveError {
 }
-exports.HiveError = HiveError;
-class HiveFarms {
+export class HiveFarms {
+    api;
     constructor(api) {
         this.api = api;
     }
@@ -20,16 +15,15 @@ class HiveFarms {
         return this.api.get('/farms');
     }
 }
-exports.HiveFarms = HiveFarms;
-class HiveAPI {
+export class HiveAPI {
+    farms;
     constructor() {
         this.farms = new HiveFarms(this);
     }
     async get(endpoint) {
-        return (0, node_fetch_1.default)(exports.SCHEME + exports.HOST + exports.BASE_PATH + endpoint)
+        return fetch(SCHEME + HOST + BASE_PATH + endpoint)
             .then(response => response.json());
     }
 }
-exports.HiveAPI = HiveAPI;
-exports.default = HiveAPI;
+export default HiveAPI;
 //# sourceMappingURL=index.js.map
